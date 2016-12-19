69ba7904eec70cb85f61884c36855092aa94cc85
agentnola <agentnola>
2016-12-19 16:02:04 -0600

    Firebase testing

--- sketch.js
+++ sketch.js
@@ -1,5 +1,25 @@
 function setup() {
   createCanvas(400, 400);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBfbJ4HgT-rr3FCxOVsXMUkRDeywitRwO4",
+    authDomain: "smartdatabase-266a9.firebaseapp.com",
+    databaseURL: "https://smartdatabase-266a9.firebaseio.com",
+    storageBucket: "smartdatabase-266a9.appspot.com",
+    messagingSenderId: "866180427680"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  var ref = database.ref("smartdatabase");
+  ref.on("value", gotData, errData);
+}
+
+function gotData(data) {
+  console.log(data.val());
+}
+
+function errData(err) {
+  console.log(err);
 }
 
 function draw() {
